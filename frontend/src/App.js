3c5aec02d7c94c31782d1be291b87e2e6659bb60
Weiland <tobias.weiland@MB0049.local>
2021-11-04 14:10:58 +0100

    remove not needed import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
-
-import './App.css';
+import {useEffect, useState} from "react";
+import {getTop10Countries} from "./service/AxiosService";
 
 function App() {
+  const [top10, setTop10] = useState()
+
+  useEffect(() => {
+    getTop10Countries()
+  })
+
   return (<div>Hello World</div>);
 }
 
