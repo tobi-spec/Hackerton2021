acef29eeb0af12760bfdf7877067df685967e588
Weiland <tobias.weiland@MB0049.local>
2021-11-04 10:08:32 +0100

    create controller route for get_country_occurency function

--- backend/FlaskController.py
+++ backend/FlaskController.py
@@ -1,8 +1,16 @@
 from flask import Flask
 
+import DataService
+from DataService import *
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def get_connection():
     return 'Hello, World! This server works fine!'
+
+
+@app.route("/country")
+def get_country_values():
+    return DataService.get_country_occurency()
