f814e26e685bf8a2abdb6aabd3abdc7da8bae008
Weiland <tobias.weiland@MB0049.local>
2021-11-04 10:08:11 +0100

    create DataService with get_country_occurency function

---
+++ backend/DataService.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import json
+
+# Data is copied into data folder manually
+path = "./data/csv_paradise_papers.2018-02-14/paradise_papers.nodes.entity.csv"
+
+dtypes = {"node_id": int,
+          "name": str,
+          "jurisdiction": str,
+          "jurisdiction_description": str,
+          "country_codes": str,
+          "countries": str,
+          "incorporation_date": str,
+          "inactivation_date": str,
+          "struck_off_date": str,
+          "closed_date": str,
+          "ibcRUC": str,
+          "status": str,
+          "company_type": str,
+          "service_provider": str,
+          "sourceID": str,
+          "valid_until": str,
+          "note": str
+          }
+
+
+def get_country_occurency():
+    df = pd.read_csv(path, dtype=dtypes)
+    df_sorted = df["countries"].value_counts().iloc[0:10]
+    return df_sorted.to_json(orient="split")
