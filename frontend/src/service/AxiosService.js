5f2609b298bb16118029ce681f40de820c3991e6
Weiland <tobias.weiland@MB0049.local>
2021-11-05 14:59:47 +0100

    adjustet axios requests

--- frontend/src/service/AxiosService.js
+++ frontend/src/service/AxiosService.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const top10OffshoreCTL = "/country?source=offshore&service_provider=Commonwealth Trust Limited"
-const top10OffshorePT = "/country?source=offshore&service_provider=Portcullis Trustnet"
-const top10Panama = "/country?source=panama"
-const top10Paradise = "/country?source=paradise"
+const top10OffshoreCTL = "/data?source=offshore&service_provider=Commonwealth Trust Limited"
+const top10OffshorePT = "/data?source=offshore&service_provider=Portcullis Trustnet"
+const top10Panama = "/data?source=panama"
+const top10Paradise = "/data?source=paradise"
 
 export const getTop10OffshoreCTL = () => {
     return axios.get(top10OffshoreCTL).then(response => response.data)
